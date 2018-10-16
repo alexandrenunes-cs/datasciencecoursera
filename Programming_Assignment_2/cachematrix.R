b2b61d1b6758d7344b0b80e0ab6af240fe101fc5
Alexandre Nunes <alexandrenunes.cs@gmail.com>
2018-10-16 11:09:47 -0300

    final edited cache file

--- Programming_Assignment_2/cachematrix.R
+++ Programming_Assignment_2/cachematrix.R
@@ -4,8 +4,18 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
+  ## @x: a square invertible matrix
+  ## return: a list containing functions to
+  ##              1. set the matrix
+  ##              2. get the matrix
+  ##              3. set the inverse
+  ##              4. get the inverse
+  ##         this list is used as the input to cacheSolve()
+  
   inv = NULL
   set = function(y) {
+    # use `<<-` to assign a value to an object in an environment 
+    # different from the current environment. 
     x <<- y
     inv <<- NULL
   }
